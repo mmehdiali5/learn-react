cf731af09aaf0eca4b4021b90f58c9199bc8381b
mmehdiali5 <kaka15121472@gmail.com>
2024-02-10 01:43:55 -0500

    New comp added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import Profile from "./components/profile";
+import MyComp from "./qcomps/firstcomp";
+//import Profile from './qcomps/profile_mistake.js'
 
 export default function Home() {
-  return (
-    <div className={styles.main}>
-        <Profile />
-    </div>
-  )
+    return (
+        <div className={styles.main}>
+            {/*<Profile/>*/}
+            {/*<Profile/>*/}
+            <MyComp/>
+        </div>
+    )
 }
